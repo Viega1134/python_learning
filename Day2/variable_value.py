366a536cd1f4e9daae4748f0aadc38a5286262c9
Viega1134 <2296090803@qq.com>
2026-07-05 22:07:05 +0800

    Add Day2 variable practice

---
+++ Day2/variable_value.py
@@ -0,0 +1,12 @@
+name = "viega"
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name = "viega1143"
+last_name = "lee"
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
+print(f"Hello, {full_name.title()}!")
+massage = f"Hello, {full_name.title()}!"
+print("language: \nPython\nC\nJavaScript")
+print("language: \n\tPython\n\tC\n\tJavaScript")
